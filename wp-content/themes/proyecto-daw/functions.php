c19f75612d3c36734dbf27fee9574395dd444e83
joseantonio_andreu <joseantonio.andreu@flat101.es>
2021-05-23 19:31:58 +0200

    Add CORS to functions.php

--- wp-content/themes/proyecto-daw/functions.php
+++ wp-content/themes/proyecto-daw/functions.php
@@ -3,21 +3,17 @@ define( 'THEME_DIR', get_stylesheet_directory() );
 define( 'THEME_URI', get_stylesheet_directory_uri() );
 define('THEME_VERSION', time());
 
-function get_current_user_id_jwt() {
-    if (!class_exists('Jwt_Auth_Public'))
-        return false;
-
-    $jwt = new \Jwt_Auth_Public('jwt-auth', '1.1.0');
-    $token = $jwt->validate_token(false);
-    if (\is_wp_error($token))
-        return false;
-    return $token->data->user->id;
+add_action('init', 'handle_preflight');
+function handle_preflight() {
+    $origin = get_http_origin();
+    if ($origin === 'https://www.proyecto-wp-api.tk') {
+        header("Access-Control-Allow-Origin: " . HEADLESS_FRONTEND_URL);
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+            status_header(200);
+            exit();
+        }
+    }
 }
-
-function jwt_auth_function($data, $user) {
-    $data['user_role'] = $user->roles;
-    $data['user_id'] = $user->ID;
-    $data['avatar']= get_avatar_url($user->ID);
-    return $data;
-}
-add_filter( 'jwt_auth_token_before_dispatch', 'jwt_auth_function', 10, 2 );
